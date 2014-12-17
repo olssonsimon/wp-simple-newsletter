e78090db400e8c85418694a913faffb14129c86f
Rasmus Kjellberg <rk@youngmedia.se>
2014-12-17 12:04:55 +0100

    Changed sd_ to sn_

--- includes/custom-subscriber-fields.php
+++ includes/custom-subscriber-fields.php
@@ -13,11 +13,11 @@ function custom_subscriber_columns( $columns ) {
 function subscriber_custom_enter_title( $input ) {
   global $post_type;
 
-  if ( is_admin() && 'sd_subscriber' == $post_type )
+  if ( is_admin() && 'sn_subscriber' == $post_type )
     return __( 'Enter email here', 'seodesign' );
   return $input;
 }
 
 // Add the hooks
-add_filter( 'manage_edit-sd_subscriber_columns', 'custom_subscriber_columns' ) ;
+add_filter( 'manage_edit-sn_subscriber_columns', 'custom_subscriber_columns' ) ;
 add_filter( 'enter_title_here', 'subscriber_custom_enter_title' );
