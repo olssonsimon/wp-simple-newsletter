8df3d15f720cdf6a02c36b78399eb7a3ee439b8e
Rasmus Kjellberg <rk@youngmedia.se>
2015-01-01 10:30:04 +0100

    css

--- views/form.php
+++ views/form.php
@@ -1,6 +1,6 @@
 <form id="newsletter" action="" method="post" name="new_subscriber">
-	<input type="submit" value="Send" class="send_mail hidden-phone" id="newsletter-submit" name="add_subscriber"> 
 	<input name="email" id="email" type="email" placeholder="Email" class="input_mail hidden-phone">
+	<input type="submit" value="Send" class="send_mail hidden-phone" id="newsletter-submit" name="add_subscriber"> 
 	<input type="hidden" name="action" value="post" />
 	<?php echo wp_nonce_field( 'new-subscriber' ); ?>
 </form>
